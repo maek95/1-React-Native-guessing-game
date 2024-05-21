becb97432e67767146d1d20c5ed35f9c6d2700d8
maek95 <mekstrom01@gmail.com>
2024-05-21 14:57:47 +0200

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,170 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import {
+  Button,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { Header } from "react-native/Libraries/NewAppScreen";
+
+/* En enkel gissningslek består av följande delar:
+
+Ett textfält för att ange en gissning
+En knapp för att skicka in gissningen
+En textvisning för att ge feedback om gissningen var för hög, för låg eller korrekt */
 
 export default function App() {
+  const [feedbackText, setFeedbackText] = useState("Feedback text");
+
+  const [number, setNumber] = useState(null);
+
+  const [isAnyNumGenarated, setIsAnyNumGenarated] = useState(false);
+
+  const [isNewNumGenerated, setIsNewNumGenerated] = useState(false);
+
+  const [isCorrect, setIsCorrect] = useState(null);
+  const [inputText, setInputText] = useState("");
+
+  const [guess, setGuess] = useState("");
+
+  useEffect(() => {
+    // console.log("hallo");
+
+    if (guess == number) {
+      //console.log("hej");
+      setFeedbackText(`${guess} is Correct!!!`);
+      setIsCorrect(true);
+      setTimeout(() => {
+        setFeedbackText("");
+        setIsCorrect(null);
+      }, 4000);
+    } else if (guess < number) {
+      setFeedbackText(`Wrong, too LOW`);
+      setIsCorrect(false);
+      setTimeout(() => {
+        setFeedbackText("");
+        setIsCorrect(null);
+      }, 4000);
+    } else if (guess > number) {
+      setFeedbackText(`Wrong, too HIGH`);
+      setIsCorrect(false);
+
+      setTimeout(() => {
+        setFeedbackText("");
+        setIsCorrect(null);
+      }, 4000);
+    }
+  }, [guess]);
+
+  function handleGenerateNum() {
+    const num = Math.floor(Math.random() * 10) + 1;
+    setIsAnyNumGenarated(true);
+
+    console.log(`generated number is ${num}`);
+
+    setIsNewNumGenerated(true);
+    setTimeout(() => {
+      setIsNewNumGenerated(false);
+    }, 2000);
+
+    setNumber(num);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {!isAnyNumGenarated && <Text style={{fontSize: "20px"}}>No number generated yet</Text>}
+      {isNewNumGenerated && <Text style={{fontSize: "20px"}}>New number generating...</Text>}
+      
+      <Pressable style={styles.button} onPress={handleGenerateNum}>
+        <Text style={{ color: "white" }}>Generate new number to Guess</Text>
+      </Pressable>
+
+     
+
+      <View
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 30,
+          gap: 8
+        }}
+      >
+        {!isNewNumGenerated && isAnyNumGenarated && (
+        <Text style={{fontSize: "20px"}}>Guess the number!</Text>
+      )}
+
+        <TextInput
+          keyboardType="number-pad"
+          onChangeText={setInputText}
+          value={inputText}
+          style={styles.input}
+          placeholder="Write guess here..."
+        ></TextInput>
+        <Pressable
+          style={styles.button}
+          onPress={() => (setGuess(inputText), setInputText(""))}
+        >
+          <Text style={{ color: "white" }}>Click</Text>
+        </Pressable>
+      </View>
+
+      <View style={{height: 200}}>
+      {!isCorrect && !(isCorrect === false) && (
+          <Text
+            style={{
+              fontSize: "32px",
+              
+              marginBottom: 20,
+              height: 100,
+              lineHeight: 0,
+              margin: 0,
+              padding: 0
+              
+            
+            }}
+          >
+           
+          </Text>
+        )}
+
+        {isCorrect && (
+          <Text
+            style={{
+              fontSize: "32px",
+              
+              marginBottom: 20,
+              color: "green",
+              height: 100,
+              lineHeight: 0,
+
+              margin: 0,
+              padding: 0
+
+            }}
+          >
+            {feedbackText}
+          </Text>
+        )}
+
+        {isCorrect === false && (
+          <Text
+            style={{
+              fontSize: "32px",
+              color: "red",
+              height: 100,
+              lineHeight: 0,
+              margin: 0,
+              padding: 0
+            }}
+          >
+            {feedbackText}
+          </Text>
+        )}
+      </View>
     </View>
   );
 }
@@ -13,8 +172,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 12,
+    /* border: "solid",
+    borderColor: "black",
+    borderWidth: 30 */
+  },
+  input: {
+    height: 40,
+    borderWidth: 1,
+    padding: 10,
+    width: 150,
+
+    borderStyle: "solid",
+    borderWidth: "1px",
+    borderColor: "lightgray",
+  },
+  button: {
+    borderRadius: 16,
+    backgroundColor: "teal",
+    borderWidth: 4,
+    borderColor: "teal",
+    borderStyle: "solid",
+    padding: 12,
+    paddingHorizontal: 32,
   },
 });
